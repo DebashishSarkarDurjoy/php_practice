1fcce4f35b6f7c9645651b5894dc32e51532560a
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2022-05-18 17:11:26 +1000

    folder structure changed

---
+++ example-1/p1_alt_3.php
@@ -0,0 +1,19 @@
+<?php $MAX_COUNT = 10 ?>
+
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="utf-8">
+        <title> Alternative syntax </title>
+    </head>
+    <body>
+        <h1>lets count</h1>
+        <p>
+            I can count to: <?php echo $MAX_COUNT; ?> <br>
+            <?php for ($i = 1; $i <= $MAX_COUNT; $i++): ?>
+                <?php echo $i . " "; ?>
+            <?php endfor; ?>
+
+        </p>
+    </body>
+</html>
